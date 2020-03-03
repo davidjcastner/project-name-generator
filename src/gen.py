bb533c7fc28c906c8c592ff2fcf167833501fe25
David Castner <davidjcastner@gmail.com>
2020-03-03 08:23:47 -0500

    switch to snake case to make more pythonic... hisssss!!

--- src/gen.py
+++ src/gen.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 
-def readfile(filename):
+def read_list(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
     if len(lines) == 0:
@@ -23,30 +23,30 @@ def titlecase(string):
     return ' '.join([word[0].upper() + word[1:] for word in string.split()])
 
 
-def generateAlliterative(prefixList, suffixMapping):
+def generate_alliterative(prefixList, suffixMapping):
     prefix = random.choice(prefixList)
     suffix = random.choice(suffixMapping[prefix[0]])
     return f'{titlecase(prefix)} {titlecase(suffix)}'
 
 
-def mapByStartingCharacter(input):
+def map_by_first_char(list_to_map):
     output = {}
-    for item in input:
+    for item in list_to_map:
         if item[0] not in output:
             output[item[0]] = []
         output[item[0]].append(item)
     return output
 
 
-def generateNProjectNames(n):
-    adjectives = readfile(f'.{os.sep}data{os.sep}adjectives.txt')
-    animals = readfile(f'.{os.sep}data{os.sep}animals.txt')
-    animalMapping = mapByStartingCharacter(animals)
+def generate_n_project_names(n):
+    adjectives = read_list(f'.{os.sep}data{os.sep}adjectives.txt')
+    animals = read_list(f'.{os.sep}data{os.sep}animals.txt')
+    animalMapping = map_by_first_char(animals)
     for x in range(n):
-        print(generateAlliterative(adjectives, animalMapping))
+        print(generate_alliterative(adjectives, animalMapping))
 
 
 if __name__ == '__main__':
     n = int(sys.argv[1])
     assert type(n) == int
-    generateNProjectNames(n)
+    generate_n_project_names(n)
