ed68f05b3ea65b42cdaf5693c5643e389006c52e
David Castner <davidjcastner@gmail.com>
2020-03-03 08:29:25 -0500

    document program

--- src/gen.py
+++ src/gen.py
@@ -1,9 +1,16 @@
+'''
+gen.py
+    takes an integer as input and creates that many
+    alliterative animal project names
+'''
+
 import os
 import random
 import sys
 
 
 def read_list(filename):
+    '''reads a text file where each line is an item in a list'''
     with open(filename, 'r') as f:
         lines = f.readlines()
     if len(lines) == 0:
@@ -16,20 +23,24 @@ def read_list(filename):
 
 
 def pascalcase(string):
+    '''converts a string to PascalCase'''
     return ''.join([word[0].upper() + word[1:] for word in string.split()])
 
 
 def titlecase(string):
+    '''converts a string to Title Case'''
     return ' '.join([word[0].upper() + word[1:] for word in string.split()])
 
 
 def generate_alliterative(prefixList, suffixMapping):
+    '''combines a list of prefixes with a suffix starting with same char'''
     prefix = random.choice(prefixList)
     suffix = random.choice(suffixMapping[prefix[0]])
     return f'{titlecase(prefix)} {titlecase(suffix)}'
 
 
 def map_by_first_char(list_to_map):
+    '''creates a dictionary where the first char is the key'''
     output = {}
     for item in list_to_map:
         if item[0] not in output:
@@ -39,6 +50,7 @@ def map_by_first_char(list_to_map):
 
 
 def generate_n_project_names(n):
+    '''full process for generating project names'''
     adjectives = read_list(f'.{os.sep}data{os.sep}adjectives.txt')
     animals = read_list(f'.{os.sep}data{os.sep}animals.txt')
     animalMapping = map_by_first_char(animals)
