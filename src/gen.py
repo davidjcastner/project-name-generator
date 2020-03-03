a7fca7498ef3a1c5196b0ca3e6b31ed2c4ad7738
David Castner <davidjcastner@gmail.com>
2020-03-03 08:19:19 -0500

    auto format

--- src/gen.py
+++ src/gen.py
@@ -2,26 +2,33 @@ import os
 import random
 import sys
 
+
 def readfile(filename):
     f = open(filename, 'r')
     lines = f.readlines()
     f.close()
-    if len(lines) == 0: return []
-    while lines[-1].strip() == '': lines = lines[:-1]
+    if len(lines) == 0:
+        return []
+    while lines[-1].strip() == '':
+        lines = lines[:-1]
     processedLines = [line.strip().lower() for line in lines]
     processedLines.sort()
     return processedLines
 
+
 def pascalcase(string):
     return ''.join([word[0].upper() + word[1:] for word in string.split()])
 
+
 def titlecase(string):
     return ' '.join([word[0].upper() + word[1:] for word in string.split()])
+
 
 def generateAlliterative(prefixList, suffixMapping):
     prefix = random.choice(prefixList)
     suffix = random.choice(suffixMapping[prefix[0]])
     return f'{titlecase(prefix)} {titlecase(suffix)}'
+
 
 def mapByStartingCharacter(input):
     output = {}
@@ -31,12 +38,14 @@ def mapByStartingCharacter(input):
         output[item[0]].append(item)
     return output
 
+
 def generateNProjectNames(n):
     adjectives = readfile(f'.{os.sep}data{os.sep}adjectives.txt')
     animals = readfile(f'.{os.sep}data{os.sep}animals.txt')
     animalMapping = mapByStartingCharacter(animals)
     for x in range(n):
         print(generateAlliterative(adjectives, animalMapping))
+
 
 if __name__ == '__main__':
     n = int(sys.argv[1])
